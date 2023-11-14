dfe120f4c1ab8e651bf15d02361876516e618d6a
yuliyaBir <zhivo4ka@gmail.com>
2023-11-14 13:36:47 +0100

    Frontend_react aanmaken

---
+++ src/main/frontend/pages/index.js
@@ -0,0 +1,19 @@
+// import "../styles/global.css"
+export default function Home(props) {
+    return (
+        <div>
+            <h1>Welcome to Helpper.</h1>
+            <p>Dat is een aplicatie voor assistenten en budgethouders.</p>
+
+            {/*<label>*/}
+            {/*    <span id="emailFout" className="fout" hidden>Verplicht in te vullen</span>*/}
+            {/*    <input id="email" required autoFocus></input>*/}
+            {/*</label>*/}
+            {/*<label>*/}
+            {/*    <span id="wachtwoordFout" className="fout" hidden>Verplicht in te vullen</span>*/}
+            {/*    <input id="wachtwoord" required autoFocus></input>*/}
+            {/*</label>*/}
+            {/*<button id="button">Inloggen</button>*/}
+        </div>
+    )
+}
